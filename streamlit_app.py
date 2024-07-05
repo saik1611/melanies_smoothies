3c63ca954485bef56a5ec67d2136c95f761f8c1c
saik1611 <63250101+saik1611@users.noreply.github.com>
2024-07-05 19:16:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ if ingredients_list :
     # st.write(my_insert_stmt)
     # st.stop()
         
-    time_to_insert =  st.button ('Submit Order')
+            time_to_insert =  st.button ('Submit Order')
 
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
